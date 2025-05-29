21a516679b7b1134d92d013d827bbda98107dc61
Nunzio <s298825@studenti.polito.it>
2025-05-29 21:09:47 +0200

    added shit

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -1,17 +1,18 @@
-import os, torch
+import os, torch, numpy as np
 from torch.utils.data import Dataset
 from torchvision.io import decode_image
 from torchvision.transforms import ToPILImage
 from datasets.labelConverter import convertLabels
 from datasets.dataAugmentation.base.baseTransformation import BaseTransformation
-
+from Extension.DAForm.countFreq import loadFrequenciesAndImages, normalizeFrequencies
 
 class GTA5(Dataset):
     _label = 'labels'
     _images = 'images'
     
     def __init__(self, path:str, convertLabels:bool=True, transform=None, transformTarget=None, 
-                    aug:BaseTransformation|list[BaseTransformation]=None)-> None:
+                    aug:BaseTransformation|list[BaseTransformation]=None, 
+                    enableProbability:bool=False)-> None:
         """
         Loads the GTA5 dataset given the path to the directory containing the dataset.
         
@@ -22,21 +23,28 @@ class GTA5(Dataset):
             transformTarget (torchvision.transforms, optional): transformations to apply to the masks. Defaults to None.
             aug (BaseTransformation|list[BaseTransformation], optional): augmentation to apply, if a list of augmentations is given
                 they will all be applied. Defaults to None.
+            enableProbability (bool, optional): if True, the images will be sampled based on the class frequencies. Defaults to False.
         """
-        
+
         super(GTA5, self).__init__()
         self._convertLabels = convertLabels
         self._transform = transform
         self._transformTarget = transformTarget
         self._aug = aug
+        self._enableProbability = enableProbability
         
+        if self._enableProbability:
+            self._img_labels, self._freq = loadFrequenciesAndImages(num_classes=19, width=1280, height=720, useOurs=False)
+            self._freq = normalizeFrequencies(self._freq)
+            
+
         imagePath = os.path.join(path, GTA5._images)
         labelPath = os.path.join(path, GTA5._label)
-        
-        self._images = {i:[os.path.join(imagePath, image), os.path.join(labelPath, image)]    
+
+        self._images = {i:[os.path.join(imagePath, image), os.path.join(labelPath, image)]
                         for i, image in enumerate(sorted(os.listdir(imagePath))) if image.endswith('.png')}
         
-
+        
     def __getitem__(self, idx:int)->torch.Tensor:
         """
         Loads the image and its corresponding label given the index.
@@ -49,8 +57,9 @@ class GTA5(Dataset):
                 - image (torch.Tensor): image as torch tensor.
                 - mask label (torch.Tensor): mask label as torch tensor.
         """
-        
         toPil = ToPILImage()
+        if self._enableProbability:
+            idx = self.__get_image_indexProb()
         
         image = decode_image(self._images[idx][0]).to(dtype=torch.uint8)
         mask =  decode_image(self._images[idx][1]).to(dtype=torch.uint8)
@@ -82,3 +91,17 @@ class GTA5(Dataset):
             int: number of images in the dataset.
         """
         return len(self._images)
+    
+    
+    def __get_image_indexProb(self, freq:list[float]=None)->int:
+        """
+        Returns the index of the image to be sampled based on the class frequencies
+        using a multinomial distribution.
+        
+        Args:
+            freq (list[float], optional): frequencies of the classes. If None, uses the class frequencies.
+        
+        Returns:
+            idx (int): index of the image to be sampled.
+        """
+        return int(np.random.choice(a=sorted(self._img_labels[int(np.random.choice(list(self._img_labels.keys()), p=freq if freq else self._freq))])))
