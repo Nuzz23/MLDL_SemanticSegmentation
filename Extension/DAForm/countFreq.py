f6cdf0aad17ff103757ac5617e3d7525a3fcaf22
Nunzio <s298825@studenti.polito.it>
2025-05-29 16:38:02 +0200

    added frequencies

---
+++ Extension/DAForm/countFreq.py
@@ -0,0 +1,62 @@
+import json
+from datasets.dataLoading import transformationGTA5
+from datasets.gta5 import GTA5
+import torch
+import os
+
+def evaluateClassFrequencies(num_classes: int=19):
+    """
+    Evaluates the class frequencies saving the results in two json files:
+    - imagesLabel.json: contains the list of images for each class
+    - frequencies.json: contains the frequency of each class
+    
+    Args:
+        num_classes (int): Number of classes in the dataset. Default is 19.
+        
+    Returns:
+    Tuple[Dict[int, List[int]], Dict[int, int]]: A tuple containing two dictionaries:
+        - imagesLabel: mapping of class labels to the list of image indices
+        - frequencies: mapping of class labels to their frequency counts
+    """
+
+    imagesLabel, frequencies = {k:set() for k in range(num_classes)}, {k:0 for k in range(num_classes)}
+    train, eval = transformationGTA5()
+
+
+    GTA6 = GTA5('./data/GTA5/', transform=train, transformTarget=eval)
+
+
+    for batch_idx in range(GTA6.__len__()):    
+        mask = GTA6.__getitem__(batch_idx)[1]
+        
+        for label in set(map(lambda x:int(x), torch.unique(mask))).difference({255}):    
+                frequencies[label] += 1
+                imagesLabel[label].add(batch_idx)
+        
+    with open("imagesLabel.json", "w") as f:
+        json.dump({k: list(v) for k, v in imagesLabel.items()}, f, indent=4)
+
+    with open("frequencies.json", "w") as f:
+        json.dump(frequencies, f, indent=4)
+    return imagesLabel, frequencies
+
+
+def loadFrequenciesAndImages(num_classes: int=19) -> tuple:
+    """
+    Load the class frequencies from the saved JSON files.
+    
+    Returns:
+        Tuple[Dict[int, List[int]], Dict[int, int]]: A tuple containing two dictionaries:
+            - imagesLabel: mapping of class labels to the list of image indices
+            - frequencies: mapping of class labels to their frequency counts
+    """
+    if not (("imagesLabel.json" in os.listdir()) and ("frequencies.json" in os.listdir())):
+        return evaluateClassFrequencies(num_classes=num_classes)
+    
+    with open("imagesLabel.json", "r") as f:
+        imagesLabel = json.load(f)
+
+    with open("frequencies.json", "r") as f:
+        frequencies = json.load(f)
+
+    return imagesLabel, frequencies
--- adversarialLearning/discriminator.py
+++ adversarialLearning/discriminator.py
@@ -1,7 +1,6 @@
 from torch import Tensor
 import torch.nn as nn
 
-
 class FCDiscriminator(nn.Module):
     def __init__(self, num_classes:int=19, ndf:int=64)->None:
         """
@@ -11,10 +10,9 @@ class FCDiscriminator(nn.Module):
             num_classes (int): Number of classes in the segmentation task.
             ndf (int): Number of filters in the first convolutional layer.
         """
-
         super(FCDiscriminator, self).__init__()
 
-        # Recieves as input a tensor of shape Bx3x1280x720 where B is the batch size, 3 is the number of channels (RGB),
+        # Receives as input a tensor of shape Bx3x1280x720 where B is the batch size, 3 is the number of channels (RGB),
         self.conv1 = nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1) # Bx 64 x 640 x 360
         self.conv2 = nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1) # Bx 128 x 320 x 180
         self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1) # Bx 256 x 160 x 90
