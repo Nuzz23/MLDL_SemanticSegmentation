c36755f709d0b884a11d730a8eca5c6a9a8c2c20
Nunzio <s298825@studenti.polito.it>
2025-07-06 12:30:31 +0200

    fixed norm in lab

--- Extension/LAB/lab.py
+++ Extension/LAB/lab.py
@@ -1,10 +1,14 @@
-import torch, numpy as np
+import torch
 from skimage import color
 
 
 class LAB():
     """
-    LAB color transfer
+    LAB color transfer.
+    It does so by converting images from RGB to LAB color space,
+    normalizing the LAB channels of the source image, and then applying
+    the mean and standard deviation of the target image's LAB channels.
+    The output is then converted back to RGB color space.
     """
     def transform(self, images: torch.Tensor, target_images: torch.Tensor) -> torch.Tensor:
         """
@@ -18,18 +22,17 @@ class LAB():
             torch.Tensor: Batch of LAB color transferred images, same shape as input.
         """
         assert images.shape == target_images.shape, "Source and target batch must have same shape"
-        images_np = images.clone().permute(0, 2, 3, 1).cpu().numpy()  # (B, H, W, C)
-        targets_np = target_images.clone().permute(0, 2, 3, 1).cpu().numpy()
-        
-        if images_np.max() > 1.1:
-            images_np, targets_np = images_np / 255.0, targets_np / 255.0
+        images, target_images = images.clone().permute(0, 2, 3, 1), target_images.clone().permute(0, 2, 3, 1)  # (B, H, W, C)
+
+        if images.max() > 1.1:
+            images, target_images = images / 255.0, target_images / 255.0
 
         out_images = []
-        for i in range(images_np.shape[0]): 
-            src_lab = color.rgb2lab(images_np[i])
-            trg_lab = color.rgb2lab(targets_np[i])
+        for i in range(images.shape[0]):
+            src_lab = color.rgb2lab(images[i])
+            trg_lab = color.rgb2lab(target_images[i])
 
-            lab_trans = ((src_lab - np.mean(src_lab, axis=(0, 1))) / np.std(src_lab, axis=(0, 1))) * np.std(trg_lab, axis=(0, 1)) + np.mean(trg_lab, axis=(0, 1))
-            out_images.append((np.clip(color.lab2rgb(lab_trans), 0, 1) * 255).astype(np.uint8))
+            lab_trans = ((src_lab - torch.mean(src_lab, axis=(0, 1))) / (torch.std(src_lab, axis=(0, 1)) + 1e-8)) * torch.std(trg_lab, axis=(0, 1)) + torch.mean(trg_lab, axis=(0, 1))
+            out_images.append((torch.clip(color.lab2rgb(lab_trans), 0, 1) * 255).byte())
 
-        return torch.from_numpy(np.stack(out_images, axis=0)).permute(0, 3, 1, 2).float() / 255.0
+        return torch.stack(out_images, axis=0).permute(0, 3, 1, 2).float() / 255.0
