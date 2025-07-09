1be16f8f953b509f5a4572a9c1be6ab1b6e20b58
Nunzio <s298825@studenti.polito.it>
2025-07-09 19:51:11 +0200

    last commit

--- stats.py
+++ stats.py
@@ -9,53 +9,53 @@ from utils import perClassIoU, meanIoULoss, print_mask
 
 
 def countFLOPS(model, width:int=1024, height:int=512)->int:
-  """
-    Counts the number of Floating Point Operations (FLOPs) in a PyTorch model.
+    """
+        Counts the number of Floating Point Operations (FLOPs) in a PyTorch model.
+
+        Args:
+            model (torch.nn.Module): The PyTorch model to count FLOPs for.
+            width (int, optional): The width of the input image. Defaults to 1024.
+            height (int, optional): The height of the input image. Defaults to 512.
+
+        Returns:
+            FLOPs (int): The total number of FLOPs in the model.
+
+    """
+    image = torch.zeros((1, 3, height, width)).cuda()
+
+    return FlopCountAnalysis(model, image).total()
+
+
+def latency(model, num_iterations:int=1000, width:int=1024, height:int=512)->list[float]:
+    """
+    Evaluates the latency and fps of a PyTorch model for a given number of iterations.
 
     Args:
-        model (torch.nn.Module): The PyTorch model to count FLOPs for.
+        model (torch.nn.Module): The PyTorch model to evaluate.
+        num_iterations (int, optional): The number of iterations to evaluate. Defaults to 1000.
         width (int, optional): The width of the input image. Defaults to 1024.
         height (int, optional): The height of the input image. Defaults to 512.
 
     Returns:
-        FLOPs (int): The total number of FLOPs in the model.
+        latency and FPS (list[float]): The list of values
+            - the mean latency of the model
+            - the standard deviation of the latency of the model
+            - the mean fps of the model
+            - the standard deviation of the fps of the model
+    """
+    image = torch.randn(1, 3, height, width).cuda()
 
-  """
-  image = torch.zeros((1, 3, height, width)).cuda()
+    latency, FPS = np.zeros(num_iterations), np.zeros(num_iterations)
 
-  return FlopCountAnalysis(model, image).total()
+    for i in range(num_iterations):
+        latency[i] = getCurrentTime()
 
+        _ = model(image)
 
-def latency(model, num_iterations:int=1000, width:int=1024, height:int=512)->list[float]:
-  """
-  Evaluates the latency and fps of a PyTorch model for a given number of iterations.
+        latency[i] = getCurrentTime() - latency[i]
+        FPS[i] = 1/latency[i]
 
-  Args:
-      model (torch.nn.Module): The PyTorch model to evaluate.
-      num_iterations (int, optional): The number of iterations to evaluate. Defaults to 1000.
-      width (int, optional): The width of the input image. Defaults to 1024.
-      height (int, optional): The height of the input image. Defaults to 512.
-
-  Returns:
-      latency and FPS (list[float]): The list of values
-        - the mean latency of the model
-        - the standard deviation of the latency of the model
-        - the mean fps of the model
-        - the standard deviation of the fps of the model
-  """
-  image = torch.randn(1, 3, height, width).cuda()
-
-  latency, FPS = np.zeros(num_iterations), np.zeros(num_iterations)
-
-  for i in range(num_iterations):
-      latency[i] = getCurrentTime()
-
-      _ = model(image)
-
-      latency[i] = getCurrentTime() - latency[i]
-      FPS[i] = 1/latency[i]
-
-  return np.mean(latency)*1000, np.std(latency)*1000, np.mean(FPS), np.std(FPS)
+    return np.mean(latency)*1000, np.std(latency)*1000, np.mean(FPS), np.std(FPS)
 
 
 
@@ -114,10 +114,10 @@ def lastEpochEvaluation(model, val_loader, criterion, enablePrint:bool=False)->f
             mIoU.append(pci[present].mean().item())
 
             if not batch_idx % 100 and enablePrint:
-              print(f'  val batch:{batch_idx} --> {loss.item()}')
-              print("val: ",meanIoULoss(mask, preds).item())
+                print(f'  val batch:{batch_idx} --> {loss.item()}')
+                print("val: ",meanIoULoss(mask, preds).item())
 
-              print_mask(preds[0].cpu(),"Pred")
-              print_mask(mask[0].cpu(),"Mask")
+                print_mask(preds[0].cpu(),"Pred")
+                print_mask(mask[0].cpu(),"Mask")
 
     return sum(mIoU)/len(mIoU) if len(mIoU) else 0, IoU/len(mIoU) if len(mIoU) else 0
